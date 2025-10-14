8b9d6a96597245600978f3816be3d3ff51de3ec2
Quynh181226 <quynh2682@icloud.com>
2025-10-14 23:25:19 +0700

    React

--- client/src/utils/axiosConfig.ts
+++ client/src/utils/axiosConfig.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-    baseURL: 'http://localhost:8080/',
-    // timeout: 5000,
-    // headers: {
-    //     'Content-Type': 'application/json',
-    // },
+    baseURL: 'http://localhost:8080/'
 });
 
 export default axiosConfig;
